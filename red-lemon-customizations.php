9459ea0e901811b08611247dd6b149eb1ebaf8ff
kosakk <kosakkas@gmail.com>
2024-10-30 13:46:37 +0200

    Updated the version of the plugin to 1.0.2

--- red-lemon-customizations.php
+++ red-lemon-customizations.php
@@ -3,7 +3,7 @@
 Plugin Name: Red Lemon Customizations
 Plugin URI: https://redlemon.gr/plugins/wp/primerides-gr
 Description: A plugin to add custom functions and code to the Prime Rides site.
-Version: 1.0.1
+Version: 1.0.2
 Requires at least: 6.6.2
 Author: Red Lemon
 Author URI: https://redlemon.gr/
